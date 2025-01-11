beef57be6d1add88d7f1561d98b4415deeb6a1f8
espelho-07 <darpanparmar1707@gmail.com>
2025-01-11 19:39:30 +0530

    LAB

---
+++ JAVA LAB/LAB-3/Lab_3C.java
@@ -0,0 +1,20 @@
+//Write a program to find maximum no from given 3 no.(without if-else).
+
+import java.util.Scanner;
+public class Lab_1C{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter First Number=");
+        int Number_1=sc.nextInt();
+
+        System.out.println("Enter Second Number=");
+        int Number_2=sc.nextInt();
+
+        System.out.println("Enter Third Number=");
+        int Number_3=sc.nextInt();
+
+        
+
+    }
+}
